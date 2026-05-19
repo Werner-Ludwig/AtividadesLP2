1f5de8fb4895911b5f0f95738d88805cbb78513c
Werner Ludwig Machado de Almeida <20251BSIFSA0020@ifba.edu.br>
2026-05-19 17:53:57 -0300

    Add files via upload

---
+++ SALA1/Pessoa.java
@@ -0,0 +1,13 @@
+package AtividadesLP2.SALA1;
+
+class Pessoa{
+
+    Carro carro = new Carro();
+
+    public void andarDeCarro(){
+        carro.acelerar();
+    }
+    public void pararOCarro(){
+        carro.parar();
+    }
+}
