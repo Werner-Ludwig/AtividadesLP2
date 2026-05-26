6c844754e2918fc79e264554e40a20b253bb6c4e
Werner Ludwig Machado de Almeida <20251BSIFSA0020@ifba.edu.br>
2026-05-25 23:34:09 -0300

    Add files via upload

--- AtividadesLP2/ATIVIDADEPRATICA2/Mercado.java
+++ AtividadesLP2/ATIVIDADEPRATICA2/Mercado.java
@@ -2,20 +2,39 @@ package AtividadesLP2.ATIVIDADEPRATICA2;
 
 public class Mercado {
 
-    public String nome;
-    public float preco;
-    public int estoque;
-    public static int qntProdutos = 0;
+    private static String nome;
+    private static float preco;
+    private static int estoque;
+    private static int qntProdutos = 0;
+    
 
     public static void main(String[] args) {
         Produtos produto1 = new Produtos("Arroz", 10.50f, 20);
-        Produtos produto2 = new Produtos("Feijão", 8.30f, 15);
-        Produtos produto3 = new Produtos("Macarrão", -5.00f, -10);
+        produto1.setNome("Arroz");
+        produto1.setPreco(10.50f);
+        produto1.setEstoque(20);
+        
 
-        System.out.println("Produto: " + produto1.nome + ", Preço: " + produto1.preco + ", Estoque: " + produto1.estoque);
-        System.out.println("Produto: " + produto2.nome + ", Preço: " + produto2.preco + ", Estoque: " + produto2.estoque);
-        System.out.println("Produto: " + produto3.nome + ", Preço: " + produto3.preco + ", Estoque: " + produto3.estoque);
-        System.out.println("Quantidade total de produtos: " + Produtos.qntProdutos);
+        Produtos produto2 = new Produtos("Feijão", 8.30f, 15);
+        produto2.setNome("Feijão");
+        produto2.setPreco(8.30f);
+        produto2.setEstoque(15);
+
+        Produtos produto3 = new Produtos("Macarrão", -5.00f, -10);
+        produto3.setNome("Macarrão");
+        produto3.setPreco(-5.00f);
+        produto3.setEstoque(-10);
+
+        System.out.println("Produto: " + produto1.getNome() + ", Preço: " + produto1.getPreco() + ", Estoque: " + produto1.getEstoque());
+        System.out.println("Valor total do estoque: " + produto1.calcularValorEstoque());
+
+        System.out.println("Produto: " + produto2.getNome() + ", Preço: " + produto2.getPreco() + ", Estoque: " + produto2.getEstoque());
+        System.out.println("Valor total do estoque: " + produto2.calcularValorEstoque());
+
+        System.out.println("Produto: " + produto3.getNome() + ", Preço: " + produto3.getPreco() + ", Estoque: " + produto3.getEstoque());
+        System.out.println("Valor total do estoque: " + produto3.calcularValorEstoque());
+
+        System.out.println("Quantidade total de produtos: " + Produtos.getQntProdutos());
     }
 
 
--- AtividadesLP2/ATIVIDADEPRATICA2/Produtos.java
+++ AtividadesLP2/ATIVIDADEPRATICA2/Produtos.java
@@ -2,10 +2,10 @@ package AtividadesLP2.ATIVIDADEPRATICA2;
 
 public class Produtos {
     
-    public String nome;
-    public float preco;
-    public int estoque;
-    public static int qntProdutos = 0;
+    private String nome;
+    private float preco;
+    private int estoque;
+    private static int qntProdutos = 0;
 
     public Produtos(String nome, float preco, int estoque) {
         this.nome = nome;
@@ -22,5 +22,50 @@ public class Produtos {
         qntProdutos++;
         
     }
+
+    public double calcularValorEstoque() {
+        return preco * estoque;
+    }
+
+    //GETTERS PARA PODER ACESSAR OS ATRIBUTOS PRIVADOS
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }   
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public static int getQntProdutos() {
+        return qntProdutos;
+    }
+   
     
+    //SETTERS PARA PODER ALTERAR OS ATRIBUTOS PRIVADOS
+    //SEGUINDO AS REGRAS DE NEGOCIO PARA PREÇO E ESTOQUE
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(float preco) {
+        if(preco < 0) {
+            this.preco = 0;
+        } else {
+            this.preco = preco;
+        }
+    }
+
+    public void setEstoque(int estoque) {
+        if(estoque < 0) {
+            this.estoque = 0;
+        } else {
+            this.estoque = estoque;
+        }
+    }
 }
